f8b10077075c53b62d36eb41c7ff736a1b20a6cb
Will Petty <will.petty@dunavant.com>
2022-08-11 07:30:13 -0500

    wpetty query params start

--- fast_api/params.py
+++ fast_api/params.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 from fastapi import FastAPI
-#wpetty to run server (from fast_api root): uvicorn main:testapp --reload
+#wpetty to run server (from fast_api root): uvicorn main:paramsapp --reload
 
 
 class ModelName(str, Enum):
@@ -9,16 +9,17 @@ class ModelName(str, Enum):
     lenet = "lenet"
     resnet = "resnet"
 
-testapp = FastAPI()
+paramsapp = FastAPI()
+
 
 #wpetty sample .get()
-@testapp.get("/home")
+@paramsapp.get("/home")
 def root():
     return {"message": "FastAPI Project"}
 
 
 #wpetty sample path parameters function
-@testapp.get("/items/{item_id}")
+@paramsapp.get("/items/{item_id}")
 #wpetty item_id: int - validation effort through type annotation, returns 422 if not an int
 def pathparams(item_id: int):
     return {"item_id": item_id}
@@ -26,18 +27,18 @@ def pathparams(item_id: int):
 
 #wpetty order of operations matters
 #wpetty if you duplicate path names, it will run the top function in codebase
-@testapp.get("/users/me")
+@paramsapp.get("/users/me")
 def read_user_me():
     return {"user_id": "the current user"}
 
 #wpetty this func will have to be after the one just above, otherwise, you will
 #wpetty never be able to access user 'me' because you are then redefining 'me' as a user_id
-@testapp.get("/users/{user_id}")
+@paramsapp.get("/users/{user_id}")
 def read_user(user_id: str):
     return {"user_id": user_id}
 
 
-@testapp.get("/models/{model_name}")
+@paramsapp.get("/models/{model_name}")
 #wpetty note Class for type annotation
 def get_model(model_name: ModelName):
     if model_name == ModelName.alexnet:
@@ -49,7 +50,7 @@ def get_model(model_name: ModelName):
     
 
 #wpetty :path here allows for a string value with full filepath to be appended to /files/
-@testapp.get("/files/{file_path:path}")
+@paramsapp.get("/files/{file_path:path}")
 #wpetty test with url: localhost:8000/files/appfiles/files/testfile.txt to see good example
 def read_file(file_path: str):
     return {"file_path": file_path}
---
+++ fast_api/query_params.py
@@ -0,0 +1,4 @@
+from fastapi import FastAPI
+
+query_app = FastAPI()
+
